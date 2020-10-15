4d73e3efcf09748ba95a3697518ca0fbe04b5f7e
Tom Monks <monksy@gmail.com>
2020-10-15 13:08:53 -0400

    Initial setup

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,4 @@
 export * from './app-user-credentials.model';
-export * from './app-user.model';
 export * from './exercise.model';
 export * from './max-attempt-item.model';
 export * from './program-statistic.model';
@@ -9,5 +8,7 @@ export * from './session-item.model';
 export * from './session-statistic.model';
 export * from './session.model';
 export * from './set-statistic.model';
+export * from './user-identity.model';
+export * from './user.model';
 export * from './week-statistic.model';
 export * from './week.model';
